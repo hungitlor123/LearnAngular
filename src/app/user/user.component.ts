8120a678a7770a00cb149422642799bc7d147044
hungdz369@gmail.com <hungdz369@gmail.com>
2024-08-10 21:42:29 +0700

    day2 - databinding

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,13 +1,39 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Student } from './user.type';
 
 @Component({
   selector: 'user',
   standalone: true,
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.scss'],
+  styleUrls: ['./user.component.css'],
+  imports: [CommonModule, FormsModule],
 })
 export class UserComponent implements OnInit {
+  email: string = 'evericks@gmail.com';
+  myClassCss: string = 'text-blue';
+
+  student: Student = { name: 'Tran Van D', age: 17 };
+
+  students: Student[] = [
+    { name: 'Phan Van A', age: 16, address: 'Ha Noi' },
+    { name: 'Cao Khac B', age: 15 },
+    { name: 'Nguyen Van C', age: 18 },
+  ];
+
+  isBlue: boolean = true;
   constructor() {}
-  name: string = 'Tokuda Pro';
+
   ngOnInit(): void {}
+  logEmailValue() {
+    console.log(this.email);
+  }
+  toggleColor() {
+    this.isBlue = !this.isBlue;
+  }
+  addStudentToArray() {
+    this.students.push(this.student);
+    console.log(this.students);
+  }
 }
